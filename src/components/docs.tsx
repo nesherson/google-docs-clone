173096e9a28563531f92b2b367add268410939c4
nesherson <nesad.omeragich27@gmail.com>
2022-05-15 23:56:19 +0200

    Add modal

--- src/components/docs.tsx
+++ src/components/docs.tsx
@@ -1,8 +1,18 @@
+import { useState } from 'react';
+
+import Modal from './modal';
+
 export default function Docs() {
+  const [isOpen, setIsOpen] = useState<boolean>(false);
+  const handleOpen = () => setIsOpen(true);
+
   return (
     <div className='docs-main'>
       <h1>Docs Clone</h1>
-      <button className='add-docs'>Add a Document</button>
+      <button className='add-docs' onClick={handleOpen}>
+        Add a Document
+      </button>
+      <Modal isOpen={isOpen} setIsOpen={setIsOpen} />
     </div>
   );
 }
